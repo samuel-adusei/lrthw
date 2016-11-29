71605e965faf20bb4807d1463b5bbdf9719df57e
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-29 20:11:43 +0000

    3a
    this is 3a as i commented  the code

---
+++ Maths-And-Numbers/exr3a.rb
@@ -0,0 +1,21 @@
+puts "I will now count my chicken:"
+#this prints out the string 
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100-25 * 3 %4}"
+#using string interpolation we are able to output the result of rooster 
+
+puts "Now i will count the eggs:"
+puts 3+2+1-5+4% 2 - 1/ 4+6
+
+puts "Is it true that 3+2 < 5-7"
+#this will be false
+
+puts "what is 3+2? #[3+2]"
+puts "what is 5-7? #{5-2}"
+
+puts "Is it greater? #{5 > -2}"
+#this is greater and will output true
+
+puts "Is it greater or equal? #{5 >= -2}"
+#this will be still be greater and output true
