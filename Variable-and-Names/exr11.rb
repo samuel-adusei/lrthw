d29f9efde4f68f61602feb63112304e1d8ee40aa
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 02:07:23 +0000

    gets.chomp

---
+++ Variable-and-Names/exr11.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+#this outs a string
+age = gets.chomp
+#created a variable asking for the user input and then adding a newline
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
