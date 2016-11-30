3546b48100873cd17a104aaa9f5cbc735e51a2aa
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 01:40:27 +0000

    learnt  /t  , // ,  """

---
+++ Variable-and-Names/exr10.rb
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in."
+#this is a variable which is equal to a string which contains a tab /t
+persian_cat = "I'm split\non a line."
+#this is a variable equal to a string which contains a newline /n
+backslash_cat = "I'm \\ a \\ cat."
+#this creates a backslash for the variable which is equal to the string
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+#this uses a 3 double quotes it makes the string longer
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
