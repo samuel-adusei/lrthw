461d83fb8cc4470bf8ad851a857cb86bef8cea84
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 12:45:28 +0000

    Create exr13.rb

---
+++ Variable-and-Names/exr13.rb
@@ -0,0 +1,9 @@
+$ ruby ex13.rb stuff things that
+Your first variable is: stuff
+Your second variable is: things
+Your third variable is: that
+$
+$ ruby ex13.rb apple orange grapefruit
+Your first variable is: apple
+Your second variable is: orange
+Your third variable is: grapefruit
