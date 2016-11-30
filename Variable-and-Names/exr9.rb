38cc2cd90d7bb0adc9f75fe1b439cfa9d4bcc930
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 01:17:32 +0000

    containing  /n and %q

---
+++ Variable-and-Names/exr9.rb
@@ -0,0 +1,16 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#this creates a string with /n which inputs a new line 
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+this outputs the string  with string interpolation 
+
+
+puts %q{
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
