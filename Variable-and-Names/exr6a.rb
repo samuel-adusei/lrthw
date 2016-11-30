ce13c21f9f2ab0a5e81e687d4a86a53644b35ead
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 00:11:45 +0000

    Create exr6a.rb

---
+++ Variable-and-Names/exr6a.rb
@@ -0,0 +1,30 @@
+types_of_people = 10
+#Created a variable equal to 10
+x = "There are #{types_of_people} types of people."
+# A variable with a string and the result of the variable using string interpolation
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#A variable is created with string interpolation  of two variables 
+puts x
+puts y
+#This outputs the string
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+#output of x variable using string interpolation
+
+hilarious = false
+#variable equal to false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+#out put of the sum of the two variables
+
+#STUDY DRILL
