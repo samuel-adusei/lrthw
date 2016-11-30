9b9657e4f33c44b709f62f8409bec64ad1cfbf3c
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-11-30 00:34:27 +0000

    Create exr7a.rb

---
+++ Variable-and-Names/exr7a.rb
@@ -0,0 +1,28 @@
+puts "Mary had a little lamb."
+#this output the string
+puts "Its fleece was white as #{'snow'}."
+#this output the string with the snow  interpolation 
+puts "And everywhere that Mary went."
+puts "." * 10  # what'd that do?
+#this will output . 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#this creates a varibe with string inside them
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+#this prints string and in one line with added variable total
+puts end7 + end8 + end9 + end10 + end11 + end12
+#this output string each time in new line 
