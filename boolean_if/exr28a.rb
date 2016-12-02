10df837f4a6e0d87423e06482906369c47204781
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-12-02 17:12:56 +0000

    if statement

---
+++ boolean_if/exr28a.rb
@@ -0,0 +1,41 @@
+people = 20
+cats = 30
+dogs = 15
+# variables equal to intergers
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+#created an if conditional based on if people variable is less than cats
+#this outputss  the string and then ends the condtional
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
+dogs += 5
+#this adds 5 to dogs variable
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+#created a conditional based on if people is less than or equal to dogs
+#this will output the string
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+
+if people == dogs
+  puts "People are dogs."
+end
