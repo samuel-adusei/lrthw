a04d5d9a19591d42af3308f560ca6f80e0cf80a1
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-12-03 00:29:05 +0000

    arrays

---
+++ loops/exr34a.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+animal = [1]
+#this will output "ruby"
+animal = [2]
+#this will output "peacock" as its the third 
+animal = [0]
+#this is the first animal and will output "bear"
+animal = [3]
+#this will output "kangaroo"
