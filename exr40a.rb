8825cb49f9718a6e318aa6c5c80dd9d1d51f939c
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2016-12-04 19:44:55 +0000

    Create exr40a.rb

---
+++ exr40a.rb
@@ -0,0 +1,13 @@
+class MyStuff
+#created a class function called "MyStuff"
+    def initialize()
+        @tangerine = "And now a thousand years between"
+    end
+
+    attr_reader :tangerine
+
+    def apple()
+        puts "I AM CLASSY APPLES!"
+    end
+
+end
